1dfaaebc3e92345db2bcc6c051df832e4c874ab6
Hideyuki-T <hide27yuki@gmail.com>
2025-04-11 13:21:41 +0900

    [add]盤面表示処理実装

--- javascript/main.js
+++ javascript/main.js
@@ -3,3 +3,10 @@ function createBoard() {
     let board = new Array(9).fill(0).map(() => new Array(9).fill(0));
     return board;
 }
+
+//盤面表示
+function printBoard(board) {
+    board.forEach(row => {
+        console.log(row.map(num => (num === 0 ? '.' : num)).join(' '));
+    });
+}
