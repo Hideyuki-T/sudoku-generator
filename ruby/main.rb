6205adf122d4e89fc887349f8d697644b0a13503
Hideyuki-T <hide27yuki@gmail.com>
2025-04-11 18:16:41 +0900

    [add]盤面表示機能実装

--- ruby/main.rb
+++ ruby/main.rb
@@ -2,3 +2,11 @@
 def create_board
     Array.new(9) {Array.new(9, 0) }
 end
+
+//盤面表示
+def print_board(board)
+    board.each do |row|
+        puts row.map { |num| num == 0 ? '.' : num.to_s }.join(" ")
+    end
+end
+
