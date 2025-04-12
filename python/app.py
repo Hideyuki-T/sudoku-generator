f3d0592a05760f336f65fcf3b7a827c9c5c401ce
Hideyuki-T <hide27yuki@gmail.com>
2025-04-12 17:49:56 +0900

    [Docker環境]ブラウザでの盤面表示を確認

--- python/app.py
+++ python/app.py
@@ -8,9 +8,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    # 完全盤を生成し、その後でパズル盤を作成
     complete_board = sudoku_generator.generate_complete_board()
     puzzle_board = sudoku_generator.remove_numbers(complete_board, removals=40)
-    return render_template("index.html", complete_board=complete_board, puzzle_board=puzzle_board)
+    # 生成結果をテンプレートに渡す
+    return render_template("index.html",
+                           complete_board=complete_board,
+                           puzzle_board=puzzle_board)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
+
