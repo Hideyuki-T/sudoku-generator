ad647800c624f4da2092d979d558f98f5db11119
Hideyuki-T <hide27yuki@gmail.com>
2025-04-12 14:55:19 +0900

    [add]Flaskアプリケーションのセットアップ

---
+++ python/app.py
@@ -0,0 +1,16 @@
+#!/user/bin/env python3
+# app.py
+
+from flask import Flask, render_template
+import sudoku_generator
+
+app = Flask(__name__)
+
+@app.rute("/")
+def index():
+    complete_board = sudoku_generator.generator_complete_board()
+    puzzle_board = sudoku_generator.remove_numbers(complete_board, removals = 40)
+    return render_template("index.html", complete_board=complete_board, puzzle_board=puzzle_board)
+
+if __name__ == "__main__"
+    app.run(debug=True)
