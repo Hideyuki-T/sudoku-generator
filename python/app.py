ab58fa08bef0f217de449b35014c46cb55cca8d4
Hideyuki-T <hide27yuki@gmail.com>
2025-04-12 15:24:39 +0900

    [add]HTMLテンプレート作成

--- python/app.py
+++ python/app.py
@@ -1,4 +1,4 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 # app.py
 
 from flask import Flask, render_template
@@ -6,11 +6,11 @@ import sudoku_generator
 
 app = Flask(__name__)
 
-@app.rute("/")
+@app.route("/")
 def index():
-    complete_board = sudoku_generator.generator_complete_board()
-    puzzle_board = sudoku_generator.remove_numbers(complete_board, removals = 40)
+    complete_board = sudoku_generator.generate_complete_board()
+    puzzle_board = sudoku_generator.remove_numbers(complete_board, removals=40)
     return render_template("index.html", complete_board=complete_board, puzzle_board=puzzle_board)
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     app.run(debug=True)
